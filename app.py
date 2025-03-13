79db04387e0d51404f2952849a98d126f185bb84
DefoNotGus <gustavo_arm21@hotmail.com>
2025-03-13 15:16:35 +0000

    Starting new web layout!

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 import joblib
 import numpy as np
 
@@ -18,32 +18,35 @@ except Exception as e:
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        try:
-            # Get form data
-            age = int(request.form.get('age', 0))
-            digital_challenges = int(request.form.get('Digital_Challenges', 0))
-            application_challenges = int(request.form.get('Application_Challenges', 0))
-            education_level = int(request.form.get('Education_Level', 0))
-            current_device = int(request.form.get('Current_Device', 0))
+        prototype = request.form.get('prototype')
 
-            # Prepare input for model
-            features = np.array([[age, digital_challenges, application_challenges, education_level, current_device]])
+        if prototype == "prototype2":
+            return render_template('prototype.html')
 
-            # Make predictions
-            text_size = rf_text_size.predict(features)[0]
-            image_size = rf_image_size.predict(features)[0]
+        elif prototype == "prototype1":
+            try:
+                # Extract user input
+                age = int(request.form.get('age', 0))
+                digital_challenges = int(request.form.get('Digital_Challenges', 0))
+                application_challenges = int(request.form.get('Application_Challenges', 0))
+                education_level = int(request.form.get('Education_Level', 0))
+                current_device = int(request.form.get('Current_Device', 0))
 
-            return render_template('result.html', text_size=text_size, image_size=image_size)
-        except ValueError:
-            return "Error: Invalid input. Please enter numerical values."
-        except Exception as e:
-            return f"Error: {e}"
+                # Prepare input for model
+                features = np.array([[age, digital_challenges, application_challenges, education_level, current_device]])
+
+                # Make predictions
+                text_size = rf_text_size.predict(features)[0]
+                image_size = rf_image_size.predict(features)[0]
+
+                return render_template('prototype.html', text_size=text_size, image_size=image_size)
+
+            except ValueError:
+                return "Error: Invalid input. Please enter numerical values."
+            except Exception as e:
+                return f"Error: {e}"
 
     return render_template('index.html')
-
-@app.route('/moodle')
-def moodle():
-    return render_template('moodle.html') #This is meant to be a mock of an educational PLatform without text suggestion options.
 
 if __name__ == '__main__':
     app.run(debug=True)
